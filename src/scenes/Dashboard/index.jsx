18e151002ed3b8e087177943b834808db40a35e5
marconi4 <lucalovato2000@gmail.com>
2018-06-13 10:11:55 +0200

    fix

--- src/scenes/Dashboard/index.jsx
+++ src/scenes/Dashboard/index.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 // import AppFrame from '../../components/AppFrame';
 //import PatientRegistration from '../PatientRegistration'
-//import LaboratoryTestPrescrizione from '../LaboratoryTestPrescrizione';
+import LaboratoryTestPrescrizione from '../LaboratoryTestPrescrizione';
 //import PatientVisit from '../PatientVisit'
-import LaboratoryTestRisultato from '../LaboratoryTestRisultato';
+//import LaboratoryTestRisultato from '../LaboratoryTestRisultato';
 const Dashboard = props => (
     // <AppFrame username="Dr. Housee" hospitalName="Hospital St. Democrito" />
     //<AppFrame username={props.username} hospitalName={props.hospitalName} />
     // <PatientRegistration username={props.username} hospitalName={props.hospitalName} />
     //<PatientVisit username={props.username} hospitalName={props.hospitalName} />
-    //<LaboratoryTestPrescrizione username={props.username} hospitalName={props.hospitalName} />
-    <LaboratoryTestRisultato username = {props.username} hospitalName={props.hospitalName} />
+    <LaboratoryTestPrescrizione username={props.username} hospitalName={props.hospitalName} />
+   // <LaboratoryTestRisultato username = {props.username} hospitalName={props.hospitalName} />
 );
 export default Dashboard;
--- src/scenes/LaboratoryTestPrescrizione/index.jsx
+++ src/scenes/LaboratoryTestPrescrizione/index.jsx
@@ -53,10 +53,7 @@ const styles = theme => ({
     boxShadow: '0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12)',
     marginBottom: '70px',
   },
-  titles: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-  },
+ 
   title: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
